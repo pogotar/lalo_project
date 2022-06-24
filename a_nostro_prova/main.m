e09fb81b5e5a24b27c2afa627bd80309c87090ee
pogotar <94491918+pogotar@users.noreply.github.com>
2022-06-24 15:30:57 +0200

    caio

--- a_nostro_prova/main.m
+++ a_nostro_prova/main.m
@@ -86,11 +86,12 @@ gain_taglio = sv(i);  % non proprio 1 ma vabbe
 omega_sampling = 2 * omega_taglio;
 T_sampling = 2*pi/omega_sampling;
 
-sysd = c2d(sys,Ts);
+sysd = c2d(sys,T_sampling);
 
 Q_d = Q4; 
 R_d = R4;
 [Klqr_d, P_d, CLP_d] = dlqr(sysd.A, sysd.B, Q_d, R_d);
 
+% sim 2
 
 
